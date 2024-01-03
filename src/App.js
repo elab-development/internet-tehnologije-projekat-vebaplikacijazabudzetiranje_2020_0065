348036f893138866a0e2f91cbe97e9a96402584e
Natalija Brankovic <natalija.brankovic01@gmail.com>
2024-01-03 13:17:46 +0100

    Dodata Login strana

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Podeli trošak</div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
